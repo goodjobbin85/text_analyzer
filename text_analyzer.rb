108928e1083ce83784326e61b51cdbb9f5903264
Thomas Dubiel <timidturtle85@gmail.com>
2017-08-01 11:40:53 -0500

    add line/character count functionality

--- text_analyzer.rb
+++ text_analyzer.rb
@@ -1,7 +1,19 @@
 #analyze text and extract data from it
+=begin
+line_count = 0
+text = ''
+File.open("oliver_twist.txt").each do |line| 
+	line_count += 1
+    line << text 
+    puts line
+end
+=end
 
-#quick test
+#readlines method. much simpler
+file = File.readlines("oliver_twist.txt")
+line_count = file.size
+text = file.join
+total_characters = text.size
 
-x = 2
-
-puts "File is work if 2 + 2 = #{x + x}"
+puts "There are #{line_count} lines in this text"
+puts "There are #{total_characters} characters in this text." 
