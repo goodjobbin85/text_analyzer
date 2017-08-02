7b9f40116cb3d01fa1da5f38fd75627e7e82dc1c
Thomas Dubiel <timidturtle85@gmail.com>
2017-08-02 09:31:35 -0500

    complete word-counting functionality

--- text_analyzer.rb
+++ text_analyzer.rb
@@ -14,6 +14,12 @@ file = File.readlines("oliver_twist.txt")
 line_count = file.size
 text = file.join
 total_characters = text.size
+text_without_spaces = text.gsub(/\s+/, '').length
+word_count = text.split(/\s+| \.| \,/) 
+#word_count = text.split the split method defaults to splitting on whitespace
+#word_count = text.scan(/\w+/)
 
 puts "There are #{line_count} lines in this text"
 puts "There are #{total_characters} characters in this text." 
+puts "There are #{text_without_spaces} characters (excluding spaces)"
+puts "There are #{word_count.size} words in this text"
